bb32415f5c1cfddf4225d7f66d1f5fce8fbd2afc
Sourcery AI <>
2022-09-24 11:24:51 +0000

    'Refactored by Sourcery'

--- django/db/transaction.py
+++ django/db/transaction.py
@@ -333,7 +333,6 @@ def _non_atomic_requests(view, using):
 def non_atomic_requests(using=None):
     if callable(using):
         return _non_atomic_requests(using, DEFAULT_DB_ALIAS)
-    else:
-        if using is None:
-            using = DEFAULT_DB_ALIAS
-        return lambda view: _non_atomic_requests(view, using)
+    if using is None:
+        using = DEFAULT_DB_ALIAS
+    return lambda view: _non_atomic_requests(view, using)
